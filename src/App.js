66db3d547909e0ccb3adfd36b61029ac64cf9b40
felipe <profissionalf.andre@gmail.com>
2024-08-30 11:55:56 -0300

    chore: Update App component with logo, form, packing list, and stats

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-function Logo() {}
-function Form() {}
-function PackingList() {}
-function Stats() {}
+function Logo() {
+  return <h1>Far Away 🌴 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list, and you already packed X (X%)💼</em>
+    </footer>
+  );
+}
