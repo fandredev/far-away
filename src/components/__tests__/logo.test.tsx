86c5f716c9454c21be0dfc88e4d17378cc02418d
felipe <profissionalf.andre@gmail.com>
2024-08-31 18:04:59 -0300

    test: add unit test for Logo component

---
+++ src/components/__tests__/logo.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Logo from "../logo";
+
+describe("Logo comoponent", () => {
+  it("should render components no errors when the component is rendered", () => {
+    render(<Logo />);
+
+    const findTitle = screen.getByText(/Far Away/i);
+
+    expect(findTitle).toBeInTheDocument();
+  });
+});
